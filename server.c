9c59a5ee564488df5b2740636871255be97647bd
AnnoyingYugoslav <16bitplayer2003@gmail.com>
2024-01-17 17:21:20 +0100

    asd

--- server.c
+++ server.c
@@ -4,13 +4,13 @@
 #include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
+//#include <sys/ipc.h>
+//#include <sys/msg.h>
 #include <sys/types.h>
 
 #define KEY 22342
 
-bool search(char text[50], int file) {
+int search(char text[50], int file) {
     switch(file) {
         case 1: // patrz, czy jest temat
         {
@@ -18,29 +18,63 @@ bool search(char text[50], int file) {
             snprintf(filename, sizeof(filename), "text/%s", text);
             int fd = open(filename, O_RDONLY);
             if (fd == -1) {
-                return false;
+                return 0;
             }
             close(fd);
-            return true;
+            return 1;
         }
-        case 2: // patrz, czy jest użytkownik
+        case 2: // patrz, czy jest użytkownik - by adress
         {
             char filename[50];
             snprintf(filename, sizeof(filename), "user/ban%s", text);
             int fd = open(filename, O_RDONLY);
             if (fd == -1) {
-                return false;
+                return 0;
             }
             close(fd);
-            return true;
+            return 1;
+        }
+        case 3: //patrz, czy jest użytkownik - by name
+        {
+            const char users[] = "users";
+            int fd = open(users, O_RDONLY);
+            char reader[50];
+            char c;
+            int counter = 0;
+            int timeout = 0;
+            while(timeout < 100000){
+                timeout++;
+                read(fd, &c, 1);
+                if(c == '\0'){
+                    close(fd);
+                    return 0;
+                }
+                else if(c == '\n'){
+                    reader[counter] = '\0';
+                    if(strcmp(reader, text) == 0){
+                        close(fd);
+                        return 1;
+                    }
+                    else{
+                        counter = 0;
+                        memset(reader, '\0', sizeof(reader)); 
+                    }
+                }
+                else{
+                    reader[counter] = c;
+                    counter++;
+                }
+            }
+            close(fd);
+            return 0;
         }
         default:
-            return true;
+            return 0;
     }
 }
 
-void sendtosub(char text[]){
-    
+void sendtosub(char text[]){ //asynchroniczne
+
 }
 
 
@@ -50,8 +84,8 @@ void login(char name[], int address) {
     snprintf(buf, sizeof(buf), "%d", address);
     if (!search(buf, 2)) {
         //add adress + name to users
-        const char[] users = "users";
-        const char[] newline = "\n";
+        const char users[] = "users";
+        const char newline[] = "\n";
         int pd = open(users, O_WRONLY);
         lseek(pd, 0, SEEK_END);
         write(pd, buf, sizeof(buf)-1);
@@ -93,15 +127,15 @@ void logout(char name[], int address){
     address = address + KEY; //Key - stala duża liczba
     snprintf(buf, sizeof(buf), "%d", address);
     if(search(buf, 2)){
-        const char[] users = "users";
-        const char[] newline = "\n";
+        const char users[] = "users";
+        const char newline[] = "\n";
         int pd = open(users, O_RDWR);
         char reader[50];
         int notdone = 1;
         int counter = 0;
         char c;
         while(notdone){
-            read(pd, *c, 1);
+            read(pd, &c, 1);
             if(c == '\n'){
                 reader[counter] == '\0';
                 if(strcmp(buf, reader)){
@@ -124,8 +158,8 @@ void logout(char name[], int address){
                 counter++;
             }
         }
-        lseek(fd, -counter, SEEK_CUR);
-        const char emp = '';
+        lseek(pd, -counter, SEEK_CUR);
+        const char emp[] = "";
         for(int i = 0; i < counter; i++){
             write(pd, emp, 1);
         }
@@ -146,7 +180,7 @@ void logout(char name[], int address){
         strcat(result2, buf);
         remove(result);
         remove(result2);
-        snd.type = 9;
+        snd.type = 11;
     }
     else{
         snd.type = 2;
@@ -163,10 +197,46 @@ void addtopic(char title[], char text[], int address, int pro){
     const char newl = '\n';
     char buf2[30];
     const char tmp[] = "text";
+    const char users[] = "users";
     snprintf(buf2, sizeof(buf2), "%d", pro);
     if(search(buf, 2)){
         //get name
-        char name[] = ;
+        char name[50];
+        int fd = open(users, O_RDONLY);
+        char c;
+        int counter2 = 0;
+        int notdone = 1;
+        while(notdone){
+            read(fd, &c, 1);
+            if(c == '\n'){
+                name[counter2] = '\0';
+                if(strcmp(name, buf) == 0){
+                    notdone = 0;
+                }
+                else{
+                    counter2 = 0;
+                    memset(name, '\0', sizeof(name));
+                }
+            }
+            else{
+                name[counter2] = c;
+                counter2++;
+            }
+        }
+        notdone = 1;
+        counter2 = 0;
+        memset(name, '\0', sizeof(name));
+        while(notdone){
+            read(fd, &c, 1);
+            if(c == '\n'){
+                notdone = 0;
+            }
+            else{
+                name[counter2] = c;
+                counter2++;
+            }
+        }
+        name[counter2] = '\0';
         if(search(title, 1)){ //jeśli już jest temat
             chdir(tmp);
             int pd = open(title, O_RDWR);
@@ -176,7 +246,7 @@ void addtopic(char title[], char text[], int address, int pro){
             int counter;
             int foundnumber = 0;
             while(notdone){
-                read(pd, *c, 1);
+                read(pd, &c, 1);
                 if(foundnumber && !(c == '\n')){
                     reader[counter] = c;
                     counter++;
@@ -198,13 +268,13 @@ void addtopic(char title[], char text[], int address, int pro){
                 }
             }
             lseek(pd, -(counter+1), SEEK_CUR);
-            write(pd, *hash, 1);
+            write(pd, &hash, 1);
             write(pd, buf2, sizeof(buf2)-1);
-            write(pd, *app, 1);
+            write(pd, &app, 1);
             write(pd, name, sizeof(name)-1);
-            write(pd, *newl, 1);
+            write(pd, &newl, 1);
             write(pd, text, sizeof(text)-1);
-            write(pd, *newl, 1);
+            write(pd, &newl, 1);
             close(pd);
             chdir("..");
             snd.type = 4;
@@ -214,13 +284,13 @@ void addtopic(char title[], char text[], int address, int pro){
             const char topics[] = "topics";
             chdir(tmp);
             int pd = creat(title, 0777);
-            write(pd, *hash, 1);
+            write(pd, &hash, 1);
             write(pd, buf2, sizeof(buf2)-1);
-            write(pd, *app, 1);
+            write(pd, &app, 1);
             write(pd, name, sizeof(name)-1);
-            write(pd, *newl, 1);
+            write(pd, &newl, 1);
             write(pd, text, sizeof(text)-1);
-            write(pd, *newl, 1);
+            write(pd, &newl, 1);
             close(pd);
             snd.type = 5;
             chdir("..");
@@ -228,7 +298,7 @@ void addtopic(char title[], char text[], int address, int pro){
             pd = open(topics, O_RDWR);
             lseek(pd, 0, SEEK_END);
             write(pd, title, sizeof(title) -1);
-            write(pd, *newl, 1);
+            write(pd, &newl, 1);
             close(pd);
         }
     }
@@ -238,11 +308,69 @@ void addtopic(char title[], char text[], int address, int pro){
     msgsnd(address, &snd, sizeof(snd), 0);
     msgctl(address, IPC_RMID, NULL);
 }
-void addsub(){
-
+void addsub(char test[], int address){
+    char buf[30];
+    address = address + KEY; //Key - stala duża liczba
+    snprintf(buf, sizeof(buf), "%d", address);
+    const char newl = '\n';
+    const char tmp[] = "user";
+    const char sub[] = "sub";
+    int subsize = sizeof(sub) + sizeof(buf);
+    char result[subsize]; //sub + name
+    strcpy(result, sub);
+    strcat(result, buf);
+    if(search(buf, 2)){
+        if(serach(test, 1)){
+            chdir(tmp);
+            int pd = open(result, O_WRONLY);
+            lseek(pd, 0, SEEK_END);
+            write(pd, test, sizeof(test)-1);
+            write(pd, &newl, 1);
+            close(pd);
+            chdir("..");
+            snd.type = 7;
+        }
+        else{
+            snd.type = 8;
+        }
+    }
+    else{
+        snd.type = 14;
+    }
+    msgsnd(address, &snd, sizeof(snd), 0);
+    msgctl(address, IPC_RMID, NULL);
 }
-void blockuser(){
-    
+void blockuser(char test[], int address){
+    char buf[30];
+    address = address + KEY; //Key - stala duża liczba
+    snprintf(buf, sizeof(buf), "%d", address);
+    const char newl = '\n';
+    const char tmp[] = "user";
+    const char sub[] = "ban";
+    int subsize = sizeof(sub) + sizeof(buf);
+    char result[subsize]; //sub + name
+    strcpy(result, sub);
+    strcat(result, buf);
+    if(search(buf, 2)){
+        if(serach(test, 3)){
+            chdir(tmp);
+            int pd = open(result, O_WRONLY);
+            lseek(pd, 0, SEEK_END);
+            write(pd, test, sizeof(test)-1);
+            write(pd, &newl, 1);
+            close(pd);
+            chdir("..");
+            snd.type = 7;
+        }
+        else{
+            snd.type = 8;
+        }
+    }
+    else{
+        snd.type = 14;
+    }
+    msgsnd(address, &snd, sizeof(snd), 0);
+    msgctl(address, IPC_RMID, NULL);
 }
 void senderr(int address){
     snd.type = 12;
@@ -250,19 +378,23 @@ void senderr(int address){
     msgctl(address, IPC_RMID, NULL);
 }
 
-int main(int argc, char* argv[]) {
-    struct msgbuf snd {
+void getmsg(char text[], int num, int address){ //synchroniczne
+
+}
+
+struct msgbuf {
         long type;
         char text[256];
         int num;
-    }
-    struct msgbuf2 rec {
+}snd;
+struct msgbuf {
         long type;
-        int adress;
+        int address;
         int pro; //prioriyty
         char top[50]; //topic or name
         char text[256];
-    }
+}rec;
+int main(int argc, char* argv[]) {
 
     int msgid = msgget(0x111111, 0666 | IPC_CREAT);
     //generate data structure
@@ -283,13 +415,13 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     int pd;
-    const char[] users = "users";
+    char users[] = "users";
     pd = creat(users, 0777);
     if(pd == -1){
         printf("Error creating list of users");
         return 1;
     }
-    const char[] topics = "topics";
+    char topics[] = "topics";
     pd = creat(topics, 0777);
     if(pd == -1){
         printf("Error creating list of topics");
@@ -309,16 +441,16 @@ int main(int argc, char* argv[]) {
                     addtopic(rec.top, rec.text, rec.address, rec.pro);
                     break;
                 case 4:
-                    //addtext();
+                    getmsg(rec.top, rec.pro, rec.address);
                     break;
                 case 5:
-                    addsub();
+                    addsub(rec.top, rec.pro);
                     break;
                 case 6:
-                    blockuser();
+                    blockuser(rec.top, rec.pro);
                     break;
                 default:
-                    senderr(int user);
+                    senderr(rec.address);
             }
         }
     }
